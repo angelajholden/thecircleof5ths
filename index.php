87c8e20a90861a78932310f7df91b779d14985a2
Angela J. Holden <angelajholden@gmail.com>
2013-09-23 22:20:30 -0500

    Changed almost everything about this site after 3 months of doing nothing with it.

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans|Roboto+Slab:100,300,400' rel='stylesheet' type='text/css'>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>The Circle of 5ths</title>
+        <meta name="description" content="A simple guide to the Circle of 5ths that includes half and whole steps, five finger patterns, tonic and dominant notes, and how to put it all together.">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/main.css">
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+<body>
+  <header>
+    <h1>The Circle of 5ths</h1>
+
+    <nav class="mainNav">
+      <ul>
+        <li><a href="#keySignatures"><span aria-hidden="true" data-icon="&#x266b;"></span>Key Signatures</a></li>
+        <li><a href="#circleDefined"><span aria-hidden="true" data-icon="&#x270e;"></span>Definition</a></li>
+        <li><a href="#halfWhole"><span aria-hidden="true" data-icon="&#x2781;"></span>Half & Whole Steps</a></li>
+        <li><a href="#fingerPatterns"><span aria-hidden="true" data-icon="&#x2266c;"></span>5 Finger Patterns</a></li>
+        <li><a href="#tonicDominant"><span aria-hidden="true" data-icon="&#x2638;"></span>Tonic & Dominant</a></li>
+        <li><a href="#putTogether"><span aria-hidden="true" data-icon="&#x2780;"></span>Putting it Together</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="pageWrap">
+
+<section id="keySignatures">
+  <article class="group">
+
+    <h2>Major & Minor Key Signatures</h2>
+
+    <div class="grid-1-4 flats">
+      <ul>
+        <li>6♭ = G♭ Major / e♭ minor</li>
+        <li>5♭ = D♭ Major / b♭ minor</li>
+        <li>4♭ = A♭ Major / f minor</li>
+        <li>3♭ = E♭ Major / c minor</li>
+        <li>2♭ = B♭ Major / g minor</li>
+        <li>1♭ = F Major / d minor</li>
+      </ul>
+    </div>
+
+    <div class="grid-1-2 circle">
+      <p><span>♮</span> (No Sharps or Flats) = C Major / a minor</p>
+      <img src="images/circle_of_5ths.png" alt="The Circle of 5ths">
+    </div>
+
+    <div class="grid-1-4 sharps">
+      <ul>
+        <li>1♯ = G Major / e minor</li>
+        <li>2♯ = D Major / b minor</li>
+        <li>3♯ = A Major / f♯ minor</li>
+        <li>4♯ = E Major / c♯ minor</li>
+        <li>5♯ = B Major / g♯ minor</li>
+        <li>6♯ = F♯ Major / d♯ minor</li>
+      </ul>
+    </div>
+  </article>
+
+    <article>
+      <h3>Music Theory for Dummies<sup>1</sup></h3>
+        <p><q>Traditionally, when you first begin taking piano lessons&mdash;especially classical piano&mdash;you’re not really taught much about key signatures beyond remembering the one or two sharps or flats you have to play in the piece of sheet music set in front of you. You’re taught to play the notes exactly as they occur in that piece, without much thought about what relationship those notes have with one another in the key.</q></p>
+        <p><q>Conversely, guitar players (even bass guitar players) are almost immediately taught about chords, scales, and finding their way around the guitar neck intuitively in their lessons. Note reading is saved for more advanced students who are already familiar with all the sounds of their instrument and where those sounds are located.<q></p>
+        <p><q>This situation is why a lot of classically trained pianists have such a hard time learning how to improvise, and why guitarists and bassists who may not even know how to read music have an edge over such students when it comes to improvising. Knowing which notes go into a specific key is essential when you’re playing with other musicians, or even if you’re noodling around on your own without the help of sheet music.</q></p>
+  </article>
+</section>
+
+<section id="circleDefined">
+
+  <h2>The Circle of 5ths Defined</h2>
+
+  <div class="">
+
+    <article>
+      <h3>Wikipedia<sup>2</sup></h3>
+      <p><q>In music theory, the circle of fifths is a visual representation of the relationships among the 12 tones of the chromatic scale, their corresponding key signatures, and the associated major and minor keys. More specifically, it is a geometrical representation of relationships among the 12 pitch classes of the chromatic scale in pitch class space.</q></p>
+      <p><q>A simple way to hear the relationship between these notes is by playing them on a piano keyboard. If you traverse the circle of fifths backwards, the notes will feel as though they fall into each other. This aural relationship is what the mathematics describe.</q></p>
+    </article>
+
+    <article>
+      <h3>Harvard Dictionary of Music<sup>3</sup></h3>
+        <p><q>The circular, clockwise arrangement of the twelve keys in an order of ascending fifths (C, D, d, a, etc.), showing that after twelve such steps the initial key is reached again. In this order, each successive key increases by one the number of sharps in the key signature. If the circle is viewed counter-clockwise (i.e., order of descending fifths: c, f, b♭, etc.), the keys follow each other with one or more flat in the signature. At one point of the circle the transition from the sharp keys to the flat keys must be made.</q></p>
+    </article>
+
+  </div>
+
+  <div>
+
+    <article>
+      <h3>Norton/Grove Encyclopedia of Music<sup>4</sup></h3>
+        <p><q>The arrangement of the tonics of the 12 major or minor keys by ascending or descending perfect 5ths, making a closed circle:</q></p>
+        <p><q>C-G-D-A-E-B-F♯ = G♭-D♭-A♭-E♭-B♭-F-C.</q></p>
+    </article>
+
+    <article>
+      <h3>Oxford Dictionary of Music<sup>5</sup></h3>
+        <p><q>Used by theorists to illustrate relative harmonic remoteness of one key from another, i.e. the number of fifths by which 2 notes are separated along the circle. If arranged clockwise by ascending (or descending) perfect fifths, the tonic of the 12 major (or minor) keys make a closed circle.</q></p>
+    </article>
+
+  </div>
+
+</section>
+
+<section id="halfWhole">
+  <h2>Half & Whole Steps</h2>
+    <article>
+      <p>Bacon ipsum dolor sit amet ribeye short loin pork loin boudin meatball tenderloin pastrami chuck pork jowl prosciutto. Meatball tri-tip turducken short loin drumstick shoulder doner shank spare ribs pork loin capicola pancetta. Meatloaf pancetta pig short loin, bacon shank sirloin strip steak filet mignon. Capicola bacon sausage, jerky biltong ham hamburger tri-tip rump t-bone. Pancetta pastrami corned beef ball tip. Short ribs bacon salami pork sausage pork loin. Meatloaf capicola pork chop andouille doner turducken meatball hamburger, chicken shoulder swine jowl ground round bresaola.</p>
+    </article>
+</section>
+
+<section id="fingerPatterns">
+  <h2>5 Finger Patterns</h2>
+    <article>
+      <p>Ribeye turkey short ribs, pork loin short loin beef ribs chicken andouille jerky turducken filet mignon strip steak boudin ground round. Venison strip steak jowl t-bone jerky ball tip. Fatback doner corned beef rump pork belly ham frankfurter bresaola pancetta strip steak boudin pig shankle spare ribs meatball. Salami t-bone ball tip jerky pastrami tongue pork loin beef ribs sausage. Rump pastrami sausage tenderloin capicola corned beef shoulder chuck ribeye pork chop pork prosciutto flank.</p>
+    </article>
+</section>
+
+<section id="tonicDominant">
+  <h2>Tonic & Dominant</h2>
+    <article>
+      <p>Prosciutto cow leberkas jerky. Jowl short loin corned beef shank brisket kielbasa. Kielbasa venison sirloin salami, rump bacon leberkas. Frankfurter brisket chicken meatball shoulder, t-bone tri-tip doner short ribs hamburger corned beef andouille biltong. Fatback pastrami ground round, sausage pork chop salami capicola.</p>
+    </article>
+</section>
+
+<section id="putTogether">
+  <h2>Putting It All Together</h2>
+    <article>
+      <p>Pork loin capicola tail cow andouille corned beef, pork pork chop swine meatball short ribs spare ribs. Short loin kielbasa brisket turkey ground round shank. Boudin flank bresaola, shoulder ground round ham hock tail leberkas shank short ribs t-bone chuck. Pork chop corned beef tri-tip, ball tip drumstick jowl flank bresaola shankle brisket.</p>
+    </article>
+  <h2>Works Cited</h2>
+    <article>
+      <ol>
+        <li>Pilhofer, Michael, and Holly Day. <cite>Music theory for dummies.</cite> Hoboken, NJ: Wiley, 2007. Print.</li>
+        <li>"Circle of fifths - Wikipedia, the free encyclopedia." <cite>Wikipedia, the free encyclopedia.</cite> N.p., n.d. Web. 6 July 2013. <a href="http://en.wikipedia.org/wiki/Circle_of_fifths" target="_blank">&lt;http://en.wikipedia.org/wiki/Circle_of_fifths&gt;</a>.</li>
+        <li>Randel, Don Michael. <cite>Harvard concise dictionary of music.</cite> Cambridge, Mass.: The Belknap Press of Harvard University Press, 1978. Print.</li>
+        <li>Sadie, Stanley, and Alison Latham. <cite>The Norton/Grove concise encyclopedia of music.</cite> Rev. and enl. ed. New York: W.W. Norton, 1994. Print.</li>
+        <li>Kennedy, Michael, and Joyce Bourne Kennedy. <cite>The concise Oxford dictionary of music.</cite> 5th ed. Oxford: Oxford University Press, 2007. Print.</li> 
+      </ol>
+    </article>
+</section>
+
+<footer>
+    <p class="copyright">&copy; <?php echo date('Y') ?> The Circle of 5ths</p>
+</footer>
+</div><?php //.pageWrap ?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/global.js"></script>
+</body>
+</html>
